50e1083bd05acc8fc627033b1ff89c5a9b221075
Aleksej579 <aleksej.eremenko579@gmail.com>
2021-07-23 19:10:59 +0300

    try add react

---
+++ assets/react-component.js
@@ -0,0 +1,14 @@
+// import enTranslations from '@shopify/polaris/locales/en.json';
+// import { AppProvider, Page, Card, Button } from '@shopify/polaris';
+
+
+// ReactDOM.render(
+//     <AppProvider i18n={enTranslations}>
+//         <Page title="Example app">
+//             <Card sectioned>
+//                 <Button onClick={() => alert('Button clicked!')}>Example button</Button>
+//             </Card>
+//         </Page>
+//     </AppProvider>,
+//     document.querySelector('#app'),
+// );
