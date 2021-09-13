f92c2134ba1b0532216e3e486e1a1146e87ccaf9
Aleksej579 <aleksej.eremenko579@gmail.com>
2021-09-13 18:32:40 +0300

    update code

--- assets/react-component.js
+++ assets/react-component.js
@@ -1,36 +1,3 @@
-// $('slider-component ul.multicolumn-list').each(function () {
-//     $(this).replaceWith($('<div class="ul_">' + this.innerHTML + '</div>'));
-// });
-// $('slider-component .ul_ li').each(function () {
-//     $(this).replaceWith($('<div class="li_">' + this.innerHTML + '</div>'));
-// });
-// $('slider-component .ul_').find('.li_').wrapAll("<div class='owl-carousel owl-theme'></div>");
-
-// $(document).ready(function () {
-//     $(".owl-carousel").owlCarousel(
-//         {
-//             items: 4,
-//             loop: true,
-//             margin: 10,
-//             autoplay: true,
-//             autoplayTimeout: 10000,
-//             responsive: {
-//                 320: {
-//                     items: 1,
-//                 },
-//                 480: {
-//                     items: 2,
-//                 },
-//                 768: {
-//                     items: 3
-//                 },
-//                 1280: {
-//                     items: 4
-//                 }
-//             }
-//         }
-//     );
-// });
 $('slider-component ul.multicolumn-list').each(function () {
     $(this).replaceWith($('<div class="ul_">' + this.innerHTML + '</div>'));
 });
@@ -64,6 +31,7 @@ $(document).ready(function () {
         }
     );
 });
+
 
 
 // Open QB
